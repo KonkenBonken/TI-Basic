ba50700108870d4b7cda71c13985ddb9d89e65c6
KonkenBonken <konrad05pettersson@gmail.com>
2022-03-30 23:32:18 +0200

    Added header and basic structure

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 const fs = require('fs');
 
-let offset = 80;
-let buffer = Buffer.alloc(84);
+function numToBuf(num, size) {
+	if (Array.isArray(num)) {
+		let b = Buffer.alloc(size || num.length);
+		let off = 0;
+		for (var n of num)
+			off += b.writeFloatLE(n, off);
+	} else {
+		let b = Buffer.alloc(size || 1);
+		b.writeFloatLE(num)
+	};
+	return b;
+}
 
-// offset += buffer.writeInt32LE( /*number,*/ offset);
+let dataSection = [ /*Array of buffers*/ ];
+dataSection.unshift(
+	numToBuf(dataSection.reduce((a, b) => a + b.length), 2)
+)
 
-// let bufArr = [];
-// bufArr.push(...Array( /*number*/ ).fill()
-// 	.map((_, index) =>
-// 		Buffer.alloc(2)
-// 	));
-
-
-// buffer = Buffer.concat([buffer, ...bufArr]);
+buffer = Buffer.concat([
+	numToBuf([42, 42, 84, 73, 56, 51, 70, 42, 26, 10, 0], 11), //header
+	Buffer.alloc(42), //Comment
+	Buffer.alloc(1), //Comment Delimiter
+	numToBuf(dataSection.reduce((a, b) => a + b.length), 2),
+	...dataSection,
+	numToBuf( /*File checksum. This is the lower 16 bits of the sum of all bytes in the data section,*/ 2)
+]);
 
 
 fs.writeFile('test.8xp', buffer, err => {
